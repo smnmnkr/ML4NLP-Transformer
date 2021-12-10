ac56bfec976917f75a87b87543986de375a7c79e
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-12-10 14:39:44 +0100

    enhanced train logging

--- transformer/__main__.py
+++ transformer/__main__.py
@@ -23,11 +23,12 @@ class Main:
     #
     def __init__(self,
                  description: str = "ML4NLP Seq2Seq Transformer",
-                 data_obj=Multi30KTranslation):
+                 data_cls=Multi30KTranslation):
 
         self.description = description
-        self.data_obj = data_obj
+        self.data_cls = data_cls
         self.config: dict = self.load_config()
+        self.train_log: list = [['epoch', 'train_loss', 'val_loss']]
 
         self.setup_pytorch()
         self.data = self.load_data()
@@ -86,7 +87,7 @@ class Main:
     #
     def load_data(self):
         print("[––– LOAD DATA ---]")
-        return self.data_obj()
+        return self.data_cls()
 
     #
     #
@@ -113,17 +114,6 @@ class Main:
     #
     def train(self):
         print("[––– BEGIN TRAINING ---]")
-
-        #  -------- format_epoch -----------
-        #
-        def format_epoch():
-            return ((
-                f"[{e}]"
-                f"\t loss(train): {train_loss:.3f}"
-                f"\t loss(val): {val_loss:.3f}"
-                f"\t time: {duration:.3f}s"
-            ))
-
         try:
             for e in range(1, self.config['training']['epochs'] + 1):
                 train_loader = self.data.get_dataloader('train')
@@ -145,16 +135,23 @@ class Main:
                     print("[––– Early stopping interrupted training ---]")
                     break
 
-                self._write_log(format_epoch())
+                # append current data to log, print according to report setting
+                self.train_log.append([e, train_loss, val_loss])
                 if e % self.config['training']['report_every'] == 0:
-                    print(format_epoch())
+                    print((f"[{e}]"
+                           f"\t loss(train): {train_loss:.3f}"
+                           f"\t loss(val): {val_loss:.3f}"
+                           f"\t time: {duration:.3f}s"
+                           ))
 
         # handle user keyboard interruption
         except KeyboardInterrupt:
             print("[––– User interrupted training, trying to proceed and save model ---]")
 
-        # load last saved model, if exists
+        # write train log, and if exists load last saved model
         finally:
+            self._write_log(self.train_log)
+
             try:
                 self.load()
                 print("[––– Loaded best model from checkpoint ---]")
