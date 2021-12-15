b778d18ca029811364a9e7e6159882fb323a1318
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-12-15 11:38:43 +0100

    moved trainable parameters to correct device

--- transformer/nn/positionalencoding.py
+++ transformer/nn/positionalencoding.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
+from transformer.utils import get_device
+
 
 class PositionalEncoding(nn.Module):
     #
@@ -19,10 +21,9 @@ class PositionalEncoding(nn.Module):
         # den = torch.exp(- torch.arange(0, emb_size, 2) * math.log(10000) / emb_size)
         # pos = torch.arange(0, maxlen).reshape(maxlen, 1)
 
-        self.pos_embedding = torch.ones((maxlen, emb_size))
         # pos_embedding[:, 0::2] = torch.sin(pos * den)
         # pos_embedding[:, 1::2] = torch.cos(pos * den)
-        self.pos_embedding = self.pos_embedding.unsqueeze(-2)
+        self.pos_embedding = torch.ones((maxlen, emb_size)).unsqueeze(-2).to(get_device())
 
         self.dropout = nn.Dropout(dropout)
         # self.register_buffer('pos_embedding', pos_embedding)
