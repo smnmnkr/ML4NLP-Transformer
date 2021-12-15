b46a434095f0fc61240606e6d69d387faaf3c6e6
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-12-15 11:35:43 +0100

    changed to experimental positionalencoding.py

--- transformer/nn/positionalencoding.py
+++ transformer/nn/positionalencoding.py
@@ -16,16 +16,16 @@ class PositionalEncoding(nn.Module):
                  maxlen: int = 5000):
         super(PositionalEncoding, self).__init__()
 
-        den = torch.exp(- torch.arange(0, emb_size, 2) * math.log(10000) / emb_size)
-        pos = torch.arange(0, maxlen).reshape(maxlen, 1)
+        # den = torch.exp(- torch.arange(0, emb_size, 2) * math.log(10000) / emb_size)
+        # pos = torch.arange(0, maxlen).reshape(maxlen, 1)
 
-        pos_embedding = torch.zeros((maxlen, emb_size))
-        pos_embedding[:, 0::2] = torch.sin(pos * den)
-        pos_embedding[:, 1::2] = torch.cos(pos * den)
-        pos_embedding = pos_embedding.unsqueeze(-2)
+        self.pos_embedding = torch.ones((maxlen, emb_size))
+        # pos_embedding[:, 0::2] = torch.sin(pos * den)
+        # pos_embedding[:, 1::2] = torch.cos(pos * den)
+        self.pos_embedding = self.pos_embedding.unsqueeze(-2)
 
         self.dropout = nn.Dropout(dropout)
-        self.register_buffer('pos_embedding', pos_embedding)
+        # self.register_buffer('pos_embedding', pos_embedding)
 
     #
     #
